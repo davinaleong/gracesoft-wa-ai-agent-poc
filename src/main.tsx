3edb3ac3644ad8869f09ac915dd88d2d2ceb3acd
Davina Leong <35887372+davinaleong@users.noreply.github.com>
2026-04-24 11:06:23 +0800

    Update branding assets and apply Montserrat font integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/500.css'
+import '@fontsource/montserrat/600.css'
+import '@fontsource/montserrat/700.css'
 import './index.css'
 import App from './App.tsx'
 
